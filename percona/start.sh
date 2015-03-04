0cd8a7e8e95076a8d18f95e3fa0e60df858a33c1
dan <dan@702010forum.com>
2015-03-04 16:49:03 +1100

    Updated dockerfile - checking out volumes now

--- percona/start.sh
+++ percona/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 33062:3306 -d percona /sbin/my_init --enable-insecure-key
+docker run -v `pwd`/data:/var/lib/mysql/data:rw -p 33062:3306 -d percona /sbin/my_init --enable-insecure-key
